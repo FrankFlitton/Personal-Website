85ba6a4cc12ac7ddd083b9868faafaee5b9a29b7
Frank Flitton <frontdesk@dynapic.io>
2018-03-11 16:14:17 -0400

    Webpack + Router change

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import BootstrapVue from "bootstrap-vue";
+import App from './App';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  router,
+  template: '<App/>',
+  components: { App },
+});
