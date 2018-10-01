451b2ebfab77b99415b25b7370476bf2f4f2812c
Frank Flitton <fflitton@gmail.com>
2018-10-01 13:35:26 -0400

    Smooth scrolling

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -29,25 +29,10 @@ export default new Router({
     if (savedPosition) {
       // savedPosition is only available for popstate navigations.
       return savedPosition
+    } else if (to.hash) { // if has a hash...
+      return { selector: to.hash } // scroll to the element
     } else {
-      const position = {}
-
-      return new Promise(resolve => {
-        // check if any matched route config has meta that requires scrolling to top
-        if (to.matched.some(m => m.meta.scrollToTop)) {
-          // coords will be used if no selector is provided,
-          // or if the selector didn't match any element.
-          position.x = 0
-          position.y = 0
-        }
-
-        // wait for the out transition to complete (if necessary)
-        this.app.$root.$once('triggerScroll', () => {
-          // if the resolved position is falsy or an empty object,
-          // will retain current scroll position.
-          resolve(position)
-        })
-      })
+      return {x: 0, y: 0}
     }
   }
 })
