58c5d4b4d91d48fa80e0657dbfc034297118e239
Frank Flitton <fflitton@gmail.com>
2021-02-13 09:33:56 -0500

    Medium Posts on Home Page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <feature-slider></feature-slider>
+    <mediumStories></mediumStories>
   </div>
 </template>
 
 <script>
 import featureSlider from '@/components/featuredSider/featureSlider.vue'
+import mediumStories from '@/components/mediumStories.vue'
 
 export default {
   name: 'home',
   components: {
-    featureSlider
+    featureSlider,
+    mediumStories
   }
 }
 </script>
