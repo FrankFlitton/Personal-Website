ce3bb1276ee551e016f23e5fe1116b9818f711cb
Frank Flitton <fflitton@gmail.com>
2021-02-13 13:27:48 -0500

    New Nuxt + Content CMS project

---
+++ ssr-frontend/nuxt.config.js
@@ -0,0 +1,136 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - ssr-frontend',
+    title: 'ssr-frontend',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    // https://www.npmjs.com/package/nuxt-github-api
+    [
+      'nuxt-github-api',
+      {
+        // token: required by the GitHub API
+        token: process.env.GITHUB_API_TOKEN,
+
+        // graphQLQuery: defaults to a search query
+        graphQLQuery: `
+          query GetUser($login: String!) {
+            user(login: $login) {
+              avatarUrl
+              pinnedItems(first: 10) {
+                totalCount
+                nodes {
+                  ... on Repository {
+                    id
+                    name
+                    homepageUrl
+                    url
+                    description
+                    openGraphImageUrl
+                    latestRelease {
+                      name
+                      tagName
+                    }
+                    stargazerCount
+                    usesCustomOpenGraphImage
+                    repositoryTopics(first: 10) {
+                      nodes {
+                        topic {
+                          name
+                          id
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              url
+              twitterUsername
+            }
+          }
+          `,
+
+        // variables: Object which includes any GraphQL variables required by your query.
+        variables: {
+          login: 'frankflitton'
+        }
+      }
+    ]
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ ssr-frontend/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row>
+    <v-col>
+      <h1>{{ page.title }}</h1>
+      <nuxt-content :document="page" />
+
+      {{ $github }}
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    // Logo,
+    // VuetifyLogo
+  },
+  async asyncData ({ $content }) {
+    const page = await $content('hello').fetch()
+
+    return {
+      page
+    }
+  }
+}
+</script>
