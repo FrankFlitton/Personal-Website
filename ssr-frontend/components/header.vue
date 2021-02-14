2dd9a881bf69ae7a6d60ea3c2db407102707ee64
Frank Flitton <fflitton@gmail.com>
2021-02-14 13:06:46 -0500

    Header Template

---
+++ ssr-frontend/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-img class="brand-logo" src="/img/branding/logo.svg"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style lang="scss">
+  .brand-logo {
+    width: 21px;
+  }
+</style>
