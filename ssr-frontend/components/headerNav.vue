2ba558bf13cb3c4d9a3658c7fb76ce70b1c6d1b3
Frank Flitton <fflitton@gmail.com>
2021-02-14 17:23:13 -0500

    Contact toggle + sticky padding

--- ssr-frontend/components/headerNav.vue
+++ ssr-frontend/components/headerNav.vue
@@ -1,25 +1,47 @@
 <template>
-  <v-container fluid class="white sticky-header">
-    <v-row>
-      <v-col class="brand-logo align-start">
-        <v-img class="d-block" src="/img/branding/logo.svg"></v-img>
-      </v-col>
-      <v-col class="align-self-center pa-0 d-none d-sm-flex">
-        <h1 class="d-flex">Frank&nbsp;JE&nbsp;Flitton</h1>
-      </v-col>
-      <v-spacer />
-      <v-col>
-          <v-btn
-            class="d-flex ml-auto"
-            fab
-            elevation="0"
-          >
-            <v-icon>mdi-coffee</v-icon>
-          </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="d-flex w-100">
+    <v-container fluid class="white sticky-header">
+      <v-row>
+        <v-col class="brand-logo align-start">
+          <v-img class="d-block" src="/img/branding/logo.svg"></v-img>
+        </v-col>
+        <v-col class="align-self-center pa-0 d-none d-sm-flex">
+          <h1 class="d-flex">Frank&nbsp;JE&nbsp;Flitton</h1>
+        </v-col>
+        <v-spacer />
+        <v-col>
+            <v-btn
+              class="d-flex ml-auto"
+              fab
+              elevation="0"
+              @click="toggle"
+            >
+              <v-icon v-if="!isVisible">mdi-coffee</v-icon>
+              <v-icon v-else>mdi-close</v-icon>
+            </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="sticky-padding">&nbsp;</div>
+  </div>
 </template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    isVisible () {
+      return this.$store.state.contact.isVisible
+    }
+  },
+  methods: {
+    ...mapMutations({
+      toggle: 'contact/toggle'
+    })
+  }
+}
+</script>
 
 <style lang="scss">
   @import "assets/styles/variables.scss";
@@ -30,5 +52,8 @@
   }
   .brand-logo {
     max-width: $header-height;
+  }
+  .sticky-padding {
+    height: $header-height
   }
 </style>
