ea0e5b60408501e92203873b63aac731ec71b6c6
Frank Flitton <fflitton@gmail.com>
2021-02-14 21:09:25 -0500

    Logo Links Home

--- ssr-frontend/components/headerNav.vue
+++ ssr-frontend/components/headerNav.vue
@@ -2,12 +2,17 @@
   <div class="d-flex w-100">
     <v-container fluid class="white sticky-header">
       <v-row>
-        <v-col class="brand-logo align-start">
-          <v-img class="d-block" src="/img/branding/logo.svg"></v-img>
-        </v-col>
-        <v-col class="align-self-center pa-0 d-none d-sm-flex">
-          <h1 class="d-flex">Frank&nbsp;JE&nbsp;Flitton</h1>
-        </v-col>
+        <nuxt-link
+          to="/"
+          class="ma-0 pa-0 d-flex"
+        >
+          <v-col class="brand-logo align-start">
+            <v-img class="d-block" src="/img/branding/logo.svg"></v-img>
+          </v-col>
+          <v-col class="align-self-center pa-0 d-none d-sm-flex">
+            <h1 class="d-flex">Frank&nbsp;JE&nbsp;Flitton</h1>
+          </v-col>
+        </nuxt-link>
         <v-spacer />
         <v-col>
             <v-btn
@@ -49,9 +54,14 @@ export default {
     top: 0;
     position: fixed;
     z-index: 1;
+    a {
+      color: black;
+      text-decoration: none;
+    }
   }
   .brand-logo {
-    max-width: $header-height;
+    max-width: $header-height !important;
+    height: $header-height !important;
   }
   .sticky-padding {
     height: $header-height
