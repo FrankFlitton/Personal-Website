461d5f7749b43f6969f44717d3af5449c64eb242
Frank Flitton <fflitton@gmail.com>
2021-02-17 22:36:29 -0500

    Formatting

--- ssr-frontend/components/contactPage.vue
+++ ssr-frontend/components/contactPage.vue
@@ -2,10 +2,7 @@
   <div class="d-flex vw-100 vh-100 contactForm">
     <v-container fluid class="h-100 red">
       <v-row class="h-100 fill-height">
-        <v-col class="black white--text py-15 left-col"
-          cols="12"
-          md="6"
-        >
+        <v-col class="black white--text py-15 left-col" cols="12" md="6">
           <v-row>
             <v-col cols="4" md="3" offset="0" offset-sm="1">
               <v-img
@@ -15,8 +12,13 @@
             </v-col>
             <v-col cols="8" md="5" offset="0" offset-sm="1">
               <h2 class="text-h3 mb-5">Let's Talk</h2>
-              <p class="d-none d-sm-block">Frank is a Fullstack UX Engineer with 8 years of experience in software engineering, UX research, and product design.</p>
-              <p class="d-none d-sm-block">Available for freelance and other opportunities.</p>
+              <p class="d-none d-sm-block">
+                Frank is a Fullstack UX Engineer with 8 years of experience in
+                software engineering, UX research, and product design.
+              </p>
+              <p class="d-none d-sm-block">
+                Available for freelance and other opportunities.
+              </p>
               <div class="social-links mt-10">
                 <v-btn
                   v-for="social in socials"
@@ -51,7 +53,7 @@ import contactForm from './contactForm'
 
 export default {
   components: {
-    contactForm
+    contactForm,
   },
   computed: {
     socials() {
@@ -72,15 +74,15 @@ export default {
         {
           icon: 'palette',
           url: 'http://behance.net/frankflitton',
-        }
+        },
       ]
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style lang="scss">
-@import "assets/styles/variables.scss";
+@import 'assets/styles/variables.scss';
 
 .contactForm {
   position: fixed;
@@ -90,19 +92,19 @@ export default {
     position: relative;
     &:after {
       $size: 30px;
-      content: " ";
+      content: ' ';
       position: absolute;
       top: 3rem;
-      right: ($size * .5 * -1);
+      right: ($size * 0.5 * -1);
       width: $size;
       height: $size;
       background: black;
       transform: rotate(45deg);
 
-      @media(max-width:767px) {
+      @media (max-width: 767px) {
         top: unset;
         right: unset;
-        bottom: ($size * .5 * -1);
+        bottom: ($size * 0.5 * -1);
         left: 3rem;
       }
     }
