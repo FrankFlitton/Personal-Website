d2505da3e1a63117c182341f372e0a1345ee7741
Frank Flitton <fflitton@gmail.com>
2023-04-24 12:04:46 -0700

    Better medium parsing

--- src/functions/medium/medium.js
+++ src/functions/medium/medium.js
@@ -1,4 +1,4 @@
-var Feed = require('rss-to-json');
+var Feed = require('rss-to-json')
 
 async function getMedium() {
   const storyFeed = await Feed.load('https://medium.com/feed/@FrankFlitton')
@@ -9,40 +9,39 @@ async function getMedium() {
     image: storyFeed.image,
   }
 
-  const stories = storyFeed.items
-    .map((item) => {
-      // Featured image from first src tag attribute
-      const featuredImage = item.content
-        .match(/src="(.*?)"/g)[0]
-        .replace(/(src=)?"/g, "")
+  const stories = [storyFeed.items[4]].map((item) => {
+    // Featured image from first src tag attribute
+    const featuredImage = item.content
+      .match(/src="(.*?)"/g)[0]
+      .replace(/(src=)?"/g, '')
 
-      // Check for subtitle
-      const checkSubtitle = item.content.indexOf("</h3><h3>")
-      const isSubtitle = checkSubtitle >= 0 && checkSubtitle < 100
+    // Check for subtitle
+    const checkSubtitle = item.content.indexOf('<h4>')
+    const isSubtitle = checkSubtitle < 150 && checkSubtitle > -1
 
-      // Extract subtitle or
-      // description from first paragraph
-      // strip HTML tags
-      const rawDescription = isSubtitle
-        ? item.content
-          .match(/<\/h3><h3>(.*?)<\/h3>/)[0]
-        : item.content
-          .match(/<p>(.*?)<\/p>/g)[0]
+    // Extract subtitle or
+    // description from first paragraph
+    // strip HTML tags
+    const rawDescription = isSubtitle
+      ? item.content.match(/<h4>(.*?)<\/h4>/g)[0]
+      : item.content.match(/<p>(.*?)<\/p>/g)[0]
 
-      const description = rawDescription
-        .replace(/<[\/]{0,1}[a-zA-Z0-9]{1,}>/g, "")
+    const description = rawDescription.replace(
+      /<[\/]{0,1}[a-zA-Z0-9]{1,}>/g,
+      ''
+    )
 
-      const story = {
-        title: item.title,
-        id: item.id,
-        url: item.url,
-        published: item.published,
-        featuredImage,
-        description,
-      }
+    const story = {
+      title: item.title,
+      id: item.id,
+      url: item.url,
+      published: item.published,
+      featuredImage,
+      description,
+    }
 
-      return story
-    })
+    return story
+  })
 
   return {
     author: author,
@@ -54,8 +53,9 @@ exports.handler = async function (event, context) {
   const response = await getMedium()
   return {
     statusCode: 200,
-    body: JSON.stringify(response, null, 2)
-  };
+    body: JSON.stringify(response, null, 2),
+  }
 }
 
-getMedium()
+// getMedium().then((res) => console.log(res.stories[0]))
+getMedium();
