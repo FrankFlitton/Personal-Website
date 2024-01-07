b7cbe574ac124bc7cf9aa817c15c378cc257715f
Frank Flitton <fflitton@gmail.com>
2024-01-06 21:39:24 -0800

    Add header link

--- frank-flitton-portfolio-site/src/components/header.tsx
+++ frank-flitton-portfolio-site/src/components/header.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <>
       <div className="font-futura z-10 w-full items-center justify-between text-sm flex backdrop-blur-md bg-white/80 fixed p-4">
-        <span className="flex items-center gap-2 mix-ble">
+        <Link className="flex items-center gap-2 mix-ble" href="/">
           <Image
             src="/img/branding/logo-transparent.svg"
             alt="Frank JE Flitton Logo"
@@ -13,7 +14,7 @@ export default function Header() {
             priority
           />
           <h1 className="text-2xl font-bold">Frank JE Flitton</h1>
-        </span>
+        </Link>
         <div className="items-end justify-center">open nav</div>
       </div>
       {/* Spacer */}
