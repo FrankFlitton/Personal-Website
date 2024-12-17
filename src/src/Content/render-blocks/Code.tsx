ff8250a154f08784a894ad24de47f9d61ea903eb
Frank Flitton <fflitton@gmail.com>
2024-12-16 23:06:23 -0800

    rm comment

--- src/src/Content/render-blocks/Code.tsx
+++ src/src/Content/render-blocks/Code.tsx
@@ -4,7 +4,6 @@
  * @returns
  */
 export const Code = (props: any) => {
-  console.log("*** code", props);
   return (
     <span>
       <code className="before:content-[''] after:content-[''] font-normal bg-slate-500/30 text-sm p-1">
